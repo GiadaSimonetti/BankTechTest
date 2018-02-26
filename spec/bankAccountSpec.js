b019d6818b598ffb0433bee5365a5b6a1ea1d38a
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-02-26 15:10:48 +0000

    Setup

---
+++ spec/bankAccountSpec.js
@@ -0,0 +1,12 @@
+alert("BOOOOOOOM")
+(function () {
+   'use strict';
+   describe('Feature test', function(){
+     var bankAccount;
+
+    beforeEach(function(){
+      bankAccount= new BankAccount();
+   });
+
+   })
+}());
